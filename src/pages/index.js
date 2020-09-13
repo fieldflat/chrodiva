ff2f4b3b8c6472c1b838621b8e4edb66247d1589
Tomonori HIRATA <hirata@sqlab.jp>
2020-09-14 00:53:35 +0900

    update: ある程度UI整えた

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import _ from "./index.module.css"
 import Navbar from "../components/js/Navbar"
 import TopMessage from "../components/js/TopMessage"
+import MiddleMessage from "../components/js/MiddleMessage"
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <TopMessage />
+      <MiddleMessage />
     </div>
   )
 }
